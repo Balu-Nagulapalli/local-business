92909379de095a8e78bd042e946e977bb4033420
balu <balunagulapalli22@gmail.com>
2026-05-30 16:00:12 +0530

    chore: remove debug scripts, finalize submission

--- server/check.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const Category = require('./models/Category.model');
-
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const cats = await Category.find({}, 'name coverImage');
-  cats.forEach(c => console.log(c.name, '→', c.coverImage || 'NO IMAGE'));
-  mongoose.disconnect();
-});
--- server/fixCategories.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const Category = require('./models/Category.model');
-
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  await Category.updateMany({}, { isActive: true });
-  console.log('✅ All categories set to active');
-  mongoose.disconnect();
-});
